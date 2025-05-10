06544dc4934178d1d3927b9521d26648e355df25
lunaticYB <84275101+lunaticYB@users.noreply.github.com>
2025-05-10 19:45:43 +0900

    [속도 아이템들]
    -일정 시간동안 속도가 증가, 감소하는 아이템 구현입니다
    -플레이어에 Player 태그를 달아주세요!
    -증가/감소 값과 지속시간은 인스펙터 변수로 조절할 수 있습니다

---
+++ Assets/Scripts/Item/Speed_Items.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class Speed_Item : MonoBehaviour
+{
+    public float speedAmount = 0.7f;  // 스피드 변화량
+    public float effectDuration = 2.0f; // 효과 지속 시간
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            
+            if (gameObject.name.Contains("Faster"))
+            {
+                speedAmount = Mathf.Abs(speedAmount);  
+                Debug.Log("속도 증가 아이템: " + speedAmount);
+                
+            }
+            else if (gameObject.name.Contains("Slower"))
+            {
+                speedAmount = -Mathf.Abs(speedAmount);  
+                Debug.Log("속도 감소 아이템: " + speedAmount);
+                
+            }
+
+            StartCoroutine (ApplyTemporarySpeed());
+
+            
+        }
+    }
+
+    private IEnumerator ApplyTemporarySpeed()
+    {
+        float originalSpeed = LoopingZMovement.speed;
+        Debug.Log("원래 속도: " + originalSpeed);
+        float newSpeed = Mathf.Clamp(originalSpeed + speedAmount, 0.1f, 1.5f);
+
+        LoopingZMovement.speed = newSpeed;
+        Debug.Log("속도 변경 적용: " + newSpeed);
+
+        yield return new WaitForSeconds(effectDuration);
+
+        LoopingZMovement.speed = originalSpeed;
+        Debug.Log("속도 복귀: " + originalSpeed);
+
+        Destroy(gameObject); 
+    }
+}
